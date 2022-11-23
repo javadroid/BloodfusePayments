90d2e8ff569824bf649b2549412c2d0d1b3618b4
Javadroid <ochigboemmanuel32@gmail.com>
2022-11-23 13:35:55 +0100

    a

--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { AppConfig, showConnect, UserSession } from "@stacks/connect";
 import SendMany from "./SendMany";
+import { Link, Navigate } from "react-router-dom";
 
 const appConfig = new AppConfig(["store_write", "publish_data"]);
 
@@ -31,13 +32,21 @@ const ConnectWallet = () => {
       //   align-items: center;
       //   justify-content: center;
       <div className="">
+        <center>
         <button className="Connect " onClick={disconnect}>
           Disconnect Wallet
         </button>
+        <br />
+        <br />
+        <button className="Connect " >
+        <Link to={`/admin`}>Admin</Link>
+        </button>
         <p>mainnet: {userSession.loadUserData().profile.stxAddress.mainnet}</p>
         <p>testnet: {userSession.loadUserData().profile.stxAddress.testnet}</p>
-
+        </center>
         <SendMany/>
+
+       
       </div>
     );
   }
